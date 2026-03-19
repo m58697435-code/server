e4816f4aa245710fbbc6018d6e0c6cd300536f87
m58697435-code <m58697435@gmail.com>
2026-03-19 15:23:33 +0500

    create new routing

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/app", (req, res) => {
     res.send("salom backendchilar");
 });
 
+app.get("/x", (req, res) => {
+    res.send("serverga routing qo'shildi");
+});
+
 app.listen(3000, () => {
     console.log("server ishladi!!!");
 });
